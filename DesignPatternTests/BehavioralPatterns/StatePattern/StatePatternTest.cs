d2ea91270514151726bd24014a01c2ad67dd8b49
Sandesh Kota <sandeshjkota@gmail.com>
2021-06-27 14:00:16 +0530

    Refactoring State Pattern Tests

--- DesignPatternTests/BehavioralPatterns/StatePattern/StatePatternTest.cs
+++ DesignPatternTests/BehavioralPatterns/StatePattern/StatePatternTest.cs
@@ -11,176 +11,179 @@ namespace DesignPatternTests.BehavioralPatterns.StatePattern
     public class StatePatternTest
     {
 
-        #region OPENED ACCOUNT TESTS
-
-        [Fact]
-        public void Opened_Account_Add_Money_Test()
+        public class OpenedAccountStatePatternTest
         {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.AddMoney(200);
+            [Fact]
+            public void Opened_Account_Add_Money_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.AddMoney(200);
 
-            Assert.Equal(300.00, account.GetAccountBalance());
+                Assert.Equal(300.00, account.GetAccountBalance());
+            }
+
+            [Fact]
+            public void Opened_Account_Get_Money_Success_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.AddMoney(200);
+                account.GetMoney(150);
+
+                Assert.Equal(150.00, account.GetAccountBalance());
+            }
+
+            [Fact]
+            public void Opened_Account_Get_Money_Failure_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+
+                Action getMoney = () => account.GetMoney(150);
+
+                var exception = Assert.Throws<InvalidOperationException>(getMoney);
+                Assert.Equal("Insufficient Funds in the account", exception.Message);
+            }
+
+            [Fact]
+            public void Opened_Account_To_Closed_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToClosedState();
+
+                Assert.Equal("CLOSED", account.GetAccountType());
+            }
+
+            [Fact]
+            public void Opened_Account_To_Withheld_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+
+                Assert.Equal("WITHHELD", account.GetAccountType());
+            }
         }
 
-        [Fact]
-        public void Opened_Account_Get_Money_Success_Test()
+        public class ClosedAccountStatePatternTest
         {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.AddMoney(200);
-            account.GetMoney(150);
 
-            Assert.Equal(150.00, account.GetAccountBalance());
+            [Fact]
+            public void Closed_Account_Add_Money_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToClosedState();
+
+                Action addMoney = () => account.AddMoney(150);
+
+                var exception = Assert.Throws<InvalidOperationException>(addMoney);
+                Assert.Equal("Cannot deposit money into a Closed account", exception.Message);
+            }
+
+            [Fact]
+            public void Closed_Account_Get_Money_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToClosedState();
+
+                Action getMoney = () => account.GetMoney(50);
+
+                var exception = Assert.Throws<InvalidOperationException>(getMoney);
+                Assert.Equal("Cannot withdraw money from a Closed account", exception.Message);
+            }
+
+            [Fact]
+            public void Closed_Account_To_Opened_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToClosedState();
+                account.ChangeToOpenedState();
+
+                Assert.Equal("OPENED", account.GetAccountType());
+            }
+
+            [Fact]
+            public void Closed_Account_To_Withheld_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToClosedState();
+
+                Action changeToWithheldState = () => account.ChangeToWithheldState();
+
+                var exception = Assert.Throws<InvalidOperationException>(changeToWithheldState);
+                Assert.Equal("Cannot move account from closed state to withheld state", exception.Message);
+            }
+
         }
 
-        [Fact]
-        public void Opened_Account_Get_Money_Failure_Test()
+        public class WithheldAccountStatePatternTest
         {
-            var account = new Account("1900675342", "BANK000765", 100.00);
 
-            Action getMoney = () => account.GetMoney(150);
+            [Fact]
+            public void Withheld_Account_Add_Money_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+                account.AddMoney(200);
 
-            var exception = Assert.Throws<InvalidOperationException>(getMoney);
-            Assert.Equal("Insufficient Funds in the account", exception.Message);
+                Assert.Equal(300.00, account.GetAccountBalance());
+            }
+
+            [Fact]
+            public void Withheld_Account_Get_Money_Success_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+                account.AddMoney(200);
+                account.GetMoney(100);
+
+                Assert.Equal(200.00, account.GetAccountBalance());
+            }
+
+            [Fact]
+            public void Withheld_Account_Get_Money_Withdraw_Limit_Failure_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+                account.AddMoney(200);
+
+                Action getMoney = () => account.GetMoney(150);
+
+                var exception = Assert.Throws<InvalidOperationException>(getMoney);
+                Assert.Equal("Withdraw limit is only 100 for this account", exception.Message);
+            }
+
+            [Fact]
+            public void Withheld_Account_Get_Money_Failure_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 50.00);
+                account.ChangeToWithheldState();
+
+                Action getMoney = () => account.GetMoney(80);
+
+                var exception = Assert.Throws<InvalidOperationException>(getMoney);
+                Assert.Equal("Insufficient Funds in the account", exception.Message);
+            }
+
+            [Fact]
+            public void Withheld_Account_To_Opened_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+                account.ChangeToOpenedState();
+
+                Assert.Equal("OPENED", account.GetAccountType());
+            }
+
+            [Fact]
+            public void Withheld_Account_To_Closed_Account_Test()
+            {
+                var account = new Account("1900675342", "BANK000765", 100.00);
+                account.ChangeToWithheldState();
+                account.ChangeToClosedState();
+
+                Assert.Equal("CLOSED", account.GetAccountType());
+            }
+
         }
 
-        [Fact]
-        public void Opened_Account_To_Closed_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToClosedState();
 
-            Assert.Equal("CLOSED", account.GetAccountType());
-        }
-
-        [Fact]
-        public void Opened_Account_To_Withheld_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-
-            Assert.Equal("WITHHELD", account.GetAccountType());
-        }
-
-        #endregion
-
-        #region CLOSED ACCOUNT TESTS
-
-        [Fact]
-        public void Closed_Account_Add_Money_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToClosedState();
-
-            Action addMoney = () => account.AddMoney(150);
-
-            var exception = Assert.Throws<InvalidOperationException>(addMoney);
-            Assert.Equal("Cannot deposit money into a Closed account", exception.Message);
-        }
-
-        [Fact]
-        public void Closed_Account_Get_Money_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToClosedState();
-
-            Action getMoney = () => account.GetMoney(50);
-
-            var exception = Assert.Throws<InvalidOperationException>(getMoney);
-            Assert.Equal("Cannot withdraw money from a Closed account", exception.Message);
-        }
-
-        [Fact]
-        public void Closed_Account_To_Opened_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToClosedState();
-            account.ChangeToOpenedState();
-
-            Assert.Equal("OPENED", account.GetAccountType());
-        }
-
-        [Fact]
-        public void Closed_Account_To_Withheld_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToClosedState();
-
-            Action changeToWithheldState = () => account.ChangeToWithheldState();
-
-            var exception = Assert.Throws<InvalidOperationException>(changeToWithheldState);
-            Assert.Equal("Cannot move account from closed state to withheld state", exception.Message);
-        }
-
-        #endregion
-
-        #region WITHHELD ACCOUNT TESTS
-
-        [Fact]
-        public void Withheld_Account_Add_Money_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-            account.AddMoney(200);
-
-            Assert.Equal(300.00, account.GetAccountBalance());
-        }
-
-        [Fact]
-        public void Withheld_Account_Get_Money_Success_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-            account.AddMoney(200);
-            account.GetMoney(100);
-
-            Assert.Equal(200.00, account.GetAccountBalance());
-        }
-
-        [Fact]
-        public void Withheld_Account_Get_Money_Withdraw_Limit_Failure_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-            account.AddMoney(200);
-
-            Action getMoney = () => account.GetMoney(150);
-
-            var exception = Assert.Throws<InvalidOperationException>(getMoney);
-            Assert.Equal("Withdraw limit is only 100 for this account", exception.Message);
-        }
-
-        [Fact]
-        public void Withheld_Account_Get_Money_Failure_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 50.00);
-            account.ChangeToWithheldState();
-
-            Action getMoney = () => account.GetMoney(80);
-
-            var exception = Assert.Throws<InvalidOperationException>(getMoney);
-            Assert.Equal("Insufficient Funds in the account", exception.Message);
-        }
-
-        [Fact]
-        public void Withheld_Account_To_Opened_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-            account.ChangeToOpenedState();
-
-            Assert.Equal("OPENED", account.GetAccountType());
-        }
-
-        [Fact]
-        public void Withheld_Account_To_Closed_Account_Test()
-        {
-            var account = new Account("1900675342", "BANK000765", 100.00);
-            account.ChangeToWithheldState();
-            account.ChangeToClosedState();
-
-            Assert.Equal("CLOSED", account.GetAccountType());
-        }
-
-        #endregion
     }
 }
