5e4d924071e381ee4977276cf0962baab4998fbd
Sandesh Kota <sandeshjkota@gmail.com>
2021-07-10 00:18:16 +0530

    Composite Pattern

--- DesignPatternTests/StructuralPatterns/CompositePattern/CompositePatternTest.cs
+++ DesignPatternTests/StructuralPatterns/CompositePattern/CompositePatternTest.cs
@@ -1,9 +1,25 @@
 ﻿using Xunit;
-using DesignPatterns.StructuralPatterns;
+using DesignPatterns.StructuralPatterns.CompositePattern;
+using System.Collections.Generic;
 
 namespace DesignPatternTests.StructuralPatterns.CompositePattern
 {
+    [Trait("Pattern", "Structural")]
     public class CompositePatternTest
     {
+        [Fact]
+        public void Composite_Pattern_Test()
+        {
+            var sandesh = new Member("Sandesh");
+            var kota = new Member("Kota");
+            var ashwin = new Member("Ashwin");
+            var kohli = new Member("Kohli");
+
+            var raina = new Manager("Raina", new List<IEmployee> { sandesh, kota });
+            var devdut = new Manager("Devdut", new List<IEmployee> { ashwin, raina });
+            var rahul = new Manager("Rahul", new List<IEmployee> { devdut, kohli });
+
+            Assert.Equal("<ul>Rahul<li><ul>Devdut<li>Ashwin</li><li><ul>Raina<li>Sandesh</li><li>Kota</li></ul></li></ul></li><li>Kohli</li></ul>", rahul.GetDetails());
+        }
     }
 }
---
+++ DesignPatterns/StructuralPatterns/CompositePattern/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns.CompositePattern
+{
+    public class Member: IEmployee
+    {
+        private readonly string _name;
+        public Member(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetDetails()
+        {
+            return this._name;
+        }
+    }
+}
